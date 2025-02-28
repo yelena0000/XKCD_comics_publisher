de64cf620b84dadbec2e16081108f3de161c5315
yelena0000 <kirillnek07081999@gmail.com>
2025-03-01 01:51:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,56 +7,49 @@ import telegram
 from environs import Env
 
 
-def download_and_save_image(image_url, filename):
-    """Скачивает и сохраняет изображение по URL."""
-    response = requests.get(image_url)
+def download_image(url, filename):
+    """Скачивает изображение по указанному URL и сохраняет его в файл."""
+    response = requests.get(url)
     response.raise_for_status()
 
     with open(filename, 'wb') as file:
         file.write(response.content)
 
 
-def get_comic_json(url):
-    """Получает JSON-ответ с комиксом XKCD."""
-    response = requests.get(url)
+def get_random_xkcd_comic():
+    """Выбирает случайный комикс XKCD, загружает его изображение и описание."""
+
+    response = requests.get('https://xkcd.com/info.0.json')
     response.raise_for_status()
-    comic_json = response.json()
-
-    return comic_json
-
-
-def fetch_random_xkcd_comic():
-    """Загружает случайный комикс XKCD и скачивает изображение."""
-    last_comic_url = 'https://xkcd.com/info.0.json'
-    last_comic_id = get_comic_json(last_comic_url)['num']
+    last_comic_id = response.json()['num']
 
     random_comic_id = random.randint(1, last_comic_id)
     random_comic_url = f'https://xkcd.com/{random_comic_id}/info.0.json'
 
-    comic_json = get_comic_json(random_comic_url)
+    response = requests.get(random_comic_url)
+    response.raise_for_status()
 
-    image_url = comic_json['img']
-    caption = comic_json['alt']
+    comic_image_url = response.json()['img']
+    comic_caption = response.json()['alt']
 
-    filename = os.path.basename(urlparse(image_url).path)
-    download_and_save_image(image_url, filename)
+    filename = os.path.basename(urlparse(comic_image_url).path)
+    download_image(comic_image_url, filename)
 
-    return filename, caption
+    return filename, comic_caption
 
 
-def send_photo_to_telegram(bot, chat_id, photo_path, caption=None):
-    """Отправляет изображение в Telegram и удаляет его после отправки."""
-    with open(photo_path, 'rb') as file:
+def send_image_to_telegram(bot, chat_id, path, caption=None):
+    """Отправляет изображение в указанный Telegram-чат."""
+    with open(path, 'rb') as file:
         bot.send_photo(
             chat_id=chat_id,
             photo=file,
             caption=caption
         )
-    os.remove(photo_path)
 
 
 def main():
-    """Главная функция: загружает XKCD и отправляет в Telegram."""
+    """Главная функция: Загружает случайный комикс XKCD и отправляет его в Telegram."""
     env = Env()
     env.read_env()
 
@@ -65,14 +58,18 @@ def main():
 
     bot = telegram.Bot(token=token)
 
-    try:
-        photo_path, caption = fetch_random_xkcd_comic()
-        send_photo_to_telegram(bot, chat_id, photo_path, caption)
+    image_path = None
 
+    try:
+        image_path, caption = get_random_xkcd_comic()
+        send_image_to_telegram(bot, chat_id, image_path, caption)
     except requests.exceptions.RequestException as error:
         print(f"Ошибка при загрузке комикса: {error}")
     except telegram.error.TelegramError as error:
         print(f"Ошибка при отправке в Telegram: {error}")
+    finally:
+        if image_path and os.path.exists(image_path):
+            os.remove(image_path)
 
 
 if __name__ == '__main__':
